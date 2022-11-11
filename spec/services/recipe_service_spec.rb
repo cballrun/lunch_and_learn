97fec5927a94293294ad28be6faf8c1771df43cb
Carter Ball <carter.k.ball@gmail.com>
2022-11-11 17:01:23 -0500

    Recipe service spec updates

--- spec/services/recipe_service_spec.rb
+++ spec/services/recipe_service_spec.rb
@@ -5,9 +5,19 @@ RSpec.describe RecipeService do
 
     it 'gets a recipe info' do
       VCR.use_cassette('thai_data') do
-        recipe_data = RecipeService.get_recipe("thailand")
-        binding.pry
-        expect(recipe_data).to be_a(Hash)
+        all_data = RecipeService.get_recipe("thailand")
+        all_recipe_data = all_data[:hits]
+        
+        expect(all_data).to be_a(Hash)
+        expect(all_data[:q]).to be_a(String)
+        expect(all_recipe_data.count).to eq(10)
+        
+        all_recipe_data.each do |recipe|
+          expect(recipe).to have_key(:recipe)
+          expect(recipe[:recipe][:label]).to be_a(String)
+          expect(recipe[:recipe][:url]).to be_a(String)
+          expect(recipe[:recipe][:image]).to be_a(String)
+        end
       end
     end
   end
