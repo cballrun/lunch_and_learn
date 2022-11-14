d331dcaebd973ff251b8cdc91ffbb4aff3d060b2
Carter Ball <carter.k.ball@gmail.com>
2022-11-14 12:15:11 -0500

    Tourist sight index happy path working as expected

--- app/controllers/api/v1/tourist_sights_controller.rb
+++ app/controllers/api/v1/tourist_sights_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::TouristSightsController < ApplicationController
 
   def index
-    
+    tourist_sights = TouristSightFacade.get_tourist_sights(params[:country])
+    render json: TouristSightSerializer.new(tourist_sights)
   end
 end
--- spec/requests/api/v1/tourist_sights_request_spec.rb
+++ spec/requests/api/v1/tourist_sights_request_spec.rb
@@ -4,12 +4,28 @@ RSpec.describe 'Tourist Sights API' do
   describe 'tourist sights index' do
     describe 'happy path' do
       it 'sends a list of tourist sights with attributes name, address, and place id' do
-        VCR.use_cassette('paris_data') do
+        VCR.use_cassette('paris_sight_data') do
           params = {country: 'france'}
 
           get "/api/v1/tourist_sights", params: params
-
+    
           expect(response).to be_successful
+
+          tourist_sight_data = JSON.parse(response.body, symbolize_names: true)
+          tourist_sights = tourist_sight_data[:data]
+
+          expect(tourist_sight_data).to be_a(Hash)
+          expect(tourist_sights).to be_a(Array)
+
+          tourist_sights.each do |sight|
+            expect(sight).to be_a(Hash)
+            expect(sight[:id]).to eq(nil)
+            expect(sight[:type]).to eq("tourist_sight")
+            expect(sight[:attributes].count).to eq(3)
+            expect(sight[:attributes][:name]).to be_a(String)
+            expect(sight[:attributes][:address]).to be_a(String)
+            expect(sight[:attributes][:place_id]).to be_a(String)
+          end
         end
       end
     end
