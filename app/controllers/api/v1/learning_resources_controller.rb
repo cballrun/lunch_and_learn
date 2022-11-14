95afcbceac63d98762aafa3ab8ffa9cb64fe8e74
Carter Ball <carter.k.ball@gmail.com>
2022-11-13 19:58:09 -0500

    Learning resource request happy path working

--- app/controllers/api/v1/learning_resources_controller.rb
+++ app/controllers/api/v1/learning_resources_controller.rb
@@ -1,6 +1,9 @@
 class Api::V1::LearningResourcesController < ApplicationController
 
   def index 
-    
+    video = VideoFacade.get_a_video(params[:country])
+    images = ImageFacade.get_images(params[:country])
+    learning_resource = LearningResource.new(video, images, params[:country])
+    render json: LearningResourceSerializer.new(learning_resource)
   end
 end
--- spec/requests/api/v1/learning_resources_request_spec.rb
+++ spec/requests/api/v1/learning_resources_request_spec.rb
@@ -10,6 +10,24 @@ RSpec.describe 'Learning Resources API' do
           get "/api/v1/learning_resources", params: params
 
           expect(response).to be_successful
+         
+          learning_resource_data = JSON.parse(response.body, symbolize_names: true)
+          learning_resource = learning_resource_data[:data]
+         
+          expect(learning_resource_data).to be_a(Hash)
+
+          expect(learning_resource).to be_a(Hash)
+          expect(learning_resource.count).to eq(3)
+          expect(learning_resource[:id]).to eq(nil)
+          expect(learning_resource[:type]).to eq("learning_resource")
+          expect(learning_resource[:attributes].count).to eq(3)
+          expect(learning_resource[:attributes][:country]).to eq("laos")
+          expect(learning_resource[:attributes][:video]).to be_a(Hash)
+          expect(learning_resource[:attributes][:video].count).to eq(2)
+          expect(learning_resource[:attributes][:video][:title]).to be_a(String)
+          expect(learning_resource[:attributes][:video][:youtube_video_id]).to be_a(String)
+          expect(learning_resource[:attributes][:images]).to be_a(Array)
+          expect(learning_resource[:attributes][:images].first).to be_a(Hash)
         end
       end
     end
