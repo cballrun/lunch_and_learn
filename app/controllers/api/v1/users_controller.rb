00cd34f2376e2db24f34303aa19ab9bf3d63947d
Carter Ball <carter.k.ball@gmail.com>
2022-11-14 17:30:01 -0500

    User creation creating a user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
+    user = User.create(user_params)
 
+  end
+
+  private 
+  def user_params
+    params.permit(:name, :email)
   end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -6,9 +6,11 @@ RSpec.describe 'Users API' do
       it 'creates a user with given valid data' do
         params = { name: "Randy Bobandy", email: "assistantsupervisor@sunnyvale.ca"}
 
+        expect(User.count).to eq(0)
         post '/api/v1/users', params: params
 
         expect(response).to be_successful
+        expect(User.count).to eq(1)
       end
     end
   end
