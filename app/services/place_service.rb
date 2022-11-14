42a1c6ac2712b10daa1f0ba215c771723df0e6af
Carter Ball <carter.k.ball@gmail.com>
2022-11-14 11:02:21 -0500

    Initial final commit

--- app/services/place_service.rb
+++ app/services/place_service.rb
@@ -1,13 +1,13 @@
 class PlaceService
   def self.get_places(place)
-    api_key = ENV['places_api_key']
+    # api_key = ENV['places_api_key']
     response = conn.get("")
     parse(response.body)
   end
 
   def self.conn
     Faraday.new("https://api.geoapify.com/v2/places") do |f|
-      
+      f.params['apiKey'] = ENV['places_api_key']
     end
   end
 
