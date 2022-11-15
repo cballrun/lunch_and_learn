2a72d089d8dafcd4580688de1d5621ec4233a5b6
Carter Ball <carter.k.ball@gmail.com>
2022-11-14 21:50:38 -0500

    Favorites migration; user and favorite model tests passing

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_14_224549) do
+ActiveRecord::Schema.define(version: 2022_11_15_024852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "country"
+    t.string "recipe_link"
+    t.string "recipe_title"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,4 +31,5 @@ ActiveRecord::Schema.define(version: 2022_11_14_224549) do
     t.string "api_key"
   end
 
+  add_foreign_key "favorites", "users"
 end
