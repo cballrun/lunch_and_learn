a8e7ebc9d52be47a328a73e3281c6668426a66de
Carter Ball <carter.k.ball@gmail.com>
2022-11-15 03:35:39 -0500

    Favorites index happy path

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,12 +1,13 @@
 class Api::V1::FavoritesController < ApplicationController
 
   def index 
-
+    user = User.find_by(api_key: params[:api_key])
+    favorites = user.favorites
+    render json: FavoriteSerializer.new(favorites), status: 201
   end
 
   def create
     user = User.find_by(api_key: params[:api_key])
-   
     if !user.nil?
       favorite = user.favorites.new(favorite_params)
       if favorite.save
@@ -15,7 +16,7 @@ class Api::V1::FavoritesController < ApplicationController
         render json: { error: error_message(favorite.errors) }, status: 400
       end
     else
-      render json: { error: "User does not exist"}, status: 400
+      render json: { error: "User does not exist." }, status: 400
     end
   end
 
--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -18,9 +18,8 @@ RSpec.describe 'Favorites API' do
 
         post '/api/v1/favorites', params: params
         success_message = JSON.parse(response.body, symbolize_names: true)
-        
+   
         expect(Favorite.count).to eq(1)
-        
         expect(response).to be_successful
         expect(response).to have_http_status(201)
         expect(success_message[:success]).to eq("Favorite added successfully.")
@@ -45,7 +44,7 @@ RSpec.describe 'Favorites API' do
 
         expect(Favorite.count).to eq(0)
         expect(response).to have_http_status(400)
-        expect(error_message[:error]).to eq("User does not exist")
+        expect(error_message[:error]).to eq("User does not exist.")
       end
 
       it 'does not create a favorite with invalid params' do
@@ -67,5 +66,45 @@ RSpec.describe 'Favorites API' do
         expect(error_message[:error]).to eq("Country can't be blank")
       end
     end
+  end
+
+  describe 'favorites index' do
+    describe 'happy path' do
+
+      it 'gets all of a users favorites' do
+        user_1 = User.create!(name: "Jim Lahey", email: "supervisor@sunnyvale.ca", api_key: SecureRandom.hex(15))
+        
+        u1_f1 = user_1.favorites.create!(country: "laos", recipe_link: "www.fakerecipelink.com/grilledcheese", recipe_title: "grilled cheese")
+        u1_f2 = user_1.favorites.create!(country: "laos", recipe_link: "www.fakerecipelink.com/friedchicken", recipe_title: "fried chicken")
+        u1_f3 = user_1.favorites.create!(country: "china", recipe_link: "www.fakerecipelink.com/burritos", recipe_title: "tasty burritos")
+
+        params = {
+          "api_key": user_1.api_key
+        }
+
+        get '/api/v1/favorites', params: params
+        favorites_data = JSON.parse(response.body, symbolize_names: true)
+        favorites_info = favorites_data[:data]
+        
+        expect(response).to be_successful
+        expect(response).to have_http_status(201)
+        expect(favorites_data).to be_a(Hash)
+        expect(favorites_info).to be_a(Array)
+        expect(favorites_info.count).to eq(3) 
+        
+        favorites_info.each do |favorite|
+          expect(favorite[:id].to_i).to be_a(Integer)
+          expect(favorite[:type]).to eq("favorite")
+          expect(favorite[:attributes].count).to eq(4)
+          expect(favorite[:attributes][:recipe_title]).to be_a(String)
+          expect(favorite[:attributes][:recipe_link]).to be_a(String)
+          expect(favorite[:attributes][:country]).to be_a(String)
+          expect(favorite[:attributes][:created_at]).to be_a(String)
+        end
+      end
+
+
+    end
+
   end
 end
