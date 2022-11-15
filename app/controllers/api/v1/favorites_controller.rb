1b65cd69d7b3e95963bf91fdd1793613d29b9624
Carter Ball <carter.k.ball@gmail.com>
2022-11-15 02:18:41 -0500

    Nonexistent user sad path

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -6,11 +6,16 @@ class Api::V1::FavoritesController < ApplicationController
 
   def create
     user = User.find_by(api_key: params[:api_key])
-    favorite = user.favorites.new(favorite_params)
-    if favorite.save
-      render json: { success: "Favorite added successfully." }, status: 201
+   
+    if !user.nil?
+      favorite = user.favorites.new(favorite_params)
+      if favorite.save
+        render json: { success: "Favorite added successfully." }, status: 201
+      else
+        render json: { message: error_message(favorite.errors) }, status: 400
+      end
     else
-      render json: { message: error_message(favorite.errors) }, status: 400
+      render json: { error: "User does not exist"}, status: 400
     end
   end
 
--- spec/requests/api/v1/favorites_request_spec.rb
+++ spec/requests/api/v1/favorites_request_spec.rb
@@ -26,5 +26,28 @@ RSpec.describe 'Favorites API' do
         expect(success_message[:success]).to eq("Favorite added successfully.")
       end
     end
+
+    describe 'sad path' do
+      it 'does not create a favorite if the user does not exist' do
+        user_1 = User.create!(name: "Randy Bobandy", email: "assistantsupervisor@sunnyvale.ca", api_key: SecureRandom.hex(15))
+
+        params = {
+          "api_key": "dfdsad88",
+          "country": "thailand",
+          "recipe_link": "https://www.tastingtable.com/.....",
+          "recipe_title": "Crab Fried Rice (Khaao Pad Bpu)"
+        }
+
+        expect(Favorite.count).to eq(0)
+
+        post '/api/v1/favorites', params: params
+        error_message = JSON.parse(response.body, symbolize_names: true)
+
+        expect(Favorite.count).to eq(0)
+      
+        expect(response).to have_http_status(400)
+        expect(error_message[:error]).to eq("User does not exist")
+      end
+    end
   end
 end
